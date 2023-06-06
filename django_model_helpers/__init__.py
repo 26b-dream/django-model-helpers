64ae97fe71875e16fb0e8f29da1c27dd07d25b9b
26b-dream <84663588+26b-dream@users.noreply.github.com>
2023-06-05 22:06:11 -0700

    Removed import that broke everything

--- django_model_helpers/__init__.py
+++ django_model_helpers/__init__.py
@@ -1,4 +1,3 @@
 from .functions import auto_unique as auto_unique
 from .get_or_new import GetOrNew as GetOrNew
-from .model_with_cast_meta import ModelWithCastMeta as ModelWithCastMeta
 from .model_with_id_and_timestamp import ModelWithIdAndTimestamp as ModelWithIdAndTimestamp
